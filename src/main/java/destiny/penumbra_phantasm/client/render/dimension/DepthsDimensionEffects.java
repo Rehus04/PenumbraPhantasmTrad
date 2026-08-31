05fb242e210e74386a155cc8f217ce4370417615
SGUDestiny <panormov2006@gmail.com>
2026-09-01 03:03:13 +0400

    Depths stuff

--- src/main/java/destiny/penumbra_phantasm/client/render/dimension/DepthsDimensionEffects.java
+++ src/main/java/destiny/penumbra_phantasm/client/render/dimension/DepthsDimensionEffects.java
@@ -86,9 +86,6 @@ public class DepthsDimensionEffects extends DimensionSpecialEffects {
 
         this.skyBuffer.bind();
         this.skyBuffer.drawWithShader(poseStack.last().pose(), projectionMatrix, RenderSystem.getShader());
-        this.lowerSkyBuffer.bind();
-        this.lowerSkyBuffer.drawWithShader(poseStack.last().pose(), projectionMatrix, RenderSystem.getShader());
-        VertexBuffer.unbind();
 
         RenderSystem.setShaderColor(1F, 1F, 1F, 1F);
         RenderSystem.enableBlend();
@@ -103,6 +100,10 @@ public class DepthsDimensionEffects extends DimensionSpecialEffects {
         this.createSprites(level);
 
         this.renderSpriteQuads(poseStack, projectionMatrix);
+
+        this.lowerSkyBuffer.bind();
+        this.lowerSkyBuffer.drawWithShader(poseStack.last().pose(), projectionMatrix, RenderSystem.getShader());
+        VertexBuffer.unbind();
 
         RenderSystem.disableBlend();
         RenderSystem.enableCull();
